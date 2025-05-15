95c0c2cd936800a8121fa660529235f13a9c983c
julian7ch <jucalderonhe@unal.edu.co>
2025-05-14 20:17:21 -0500

    solución punto 1

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -4,9 +4,16 @@ datos requeridos se encuentran en el archivo data.csv. En este laboratorio
 solo puede utilizar las funciones y librerias basicas de python. No puede
 utilizar pandas, numpy o scipy.
 """
-
-
+import csv
 def pregunta_01():
+    with open('./files/input/data.csv', newline='', encoding='utf-8') as archivo:
+        lector = csv.reader(archivo, delimiter='\t')
+        contador = 0
+        for fila in lector:
+            contador +=  int(fila[1])
+        print(contador)
+        return contador
+
     """
     Retorne la suma de la segunda columna.
 
@@ -14,3 +21,4 @@ def pregunta_01():
     214
 
     """
+pregunta_01()
