299c0e8fda4282f39aeae186fc8de17b3b562083
julian7ch <jucalderonhe@unal.edu.co>
2025-05-14 22:58:03 -0500

    solución ejercicio 2

--- homework/pregunta_02.py
+++ homework/pregunta_02.py
@@ -4,9 +4,32 @@ datos requeridos se encuentran en el archivo data.csv. En este laboratorio
 solo puede utilizar las funciones y librerias basicas de python. No puede
 utilizar pandas, numpy o scipy.
 """
-
+import csv
 
 def pregunta_02():
+    with open('./files/input/data.csv', newline='', encoding= 'utf-8') as archivo:
+        lectura = csv.reader(archivo, delimiter = '\t')
+        lista = []
+        contador = {}
+
+        for elemento in lectura:
+            lista.append(elemento[0])
+
+
+        for letra in lista:
+            if letra in contador:
+                contador[letra] += 1
+            else:
+                contador[letra] = 1
+        
+    
+        conteoLetras = sorted(list(contador.items()))
+        
+        print(lista)
+        print(conteoLetras)
+
+        return conteoLetras
+        
     """
     Retorne la cantidad de registros por cada letra de la primera columna como
     la lista de tuplas (letra, cantidad), ordendas alfabéticamente.
@@ -15,3 +38,4 @@ def pregunta_02():
     [('A', 8), ('B', 7), ('C', 5), ('D', 6), ('E', 14)]
 
     """
+pregunta_02()
